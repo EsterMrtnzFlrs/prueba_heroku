3ee351461be8d25bb104c2c8e325c87d424f373f
Jano Soler <jano.soler@xarxatecactiva.com>
2022-02-08 11:23:35 +0100

    PORT added

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express =  require('express');
 
 const app = express(); 
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
 
 app.get("/", (req, res)=>{
     res.sendFile("index.html");
 })
 
-app.listen(3000, ()=> console.log("Wordle is listening at port 3000..."))
+app.listen(port, ()=> console.log(`Wordle is listening at port ${port}...`))
